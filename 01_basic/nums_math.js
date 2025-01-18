3c26fff2b520b92136f37d1bc57bdb4ebf998390
Harshal-2004 <153942492+Harshal-2004@users.noreply.github.com>
2025-01-18 18:37:56 +0000

    math and num

---
+++ 01_basic/nums_math.js
@@ -0,0 +1,13 @@
+// const score =50
+// console.log(score)
+
+// const number = 123.5678
+// console.log(number.toPrecision(4))
+
+// #====================MATHS================================
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6799))
+
+
+console.log(Math.min(4,6,7,9,9,13))
---
+++ 01_basic/string.js
@@ -0,0 +1,9 @@
+let name = "harshal"
+let repocount =10
+
+console.log(`Hello myy name is ${name} and my repocount is ${repocount}`);
+
+
+const str = "            harshal          "
+console.log(str)
+console.log(str.trim())
