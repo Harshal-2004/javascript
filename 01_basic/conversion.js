502f80abeee76265298d0654d56eb5dd9070c25f
Harshal-2004 <153942492+Harshal-2004@users.noreply.github.com>
2025-01-18 17:19:40 +0000

    container config done

---
+++ 01_basic/conversion.js
@@ -0,0 +1,10 @@
+let somenumber =33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
+
+
+let str1 = "hars"
+let str2 = "halll"
+console.log(str1+str2)
---
+++ 01_basic/variables.js
@@ -0,0 +1,6 @@
+let account_email = "harshal@gmail.com"
+const accountId = 1343
+var acccpassword="1334"
+account = "hsho" ;
+
+console.table( [accountId , acccpassword , account_email])
