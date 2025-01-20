d5cfe3826739de6dcbc14d5307e1038ea7e182dd
Harshal-2004 <153942492+Harshal-2004@users.noreply.github.com>
2025-01-20 09:50:49 +0000

    basic 2

---
+++ 02_basic/array1.js
@@ -0,0 +1,11 @@
+const myArr = [0, 1, 2, 3, 4, 5];
+const myheros = ["shaktiman", "hanuman"];
+console.log(myArr[0]);
+
+//array methods
+
+// myArr.push(6)
+// console.log(myArr)
+
+// myArr.pop(6)
+// console.log(myArr)
---
+++ 02_basic/objects.js
@@ -0,0 +1,8 @@
+//declaring object 
+
+const JsUser ={
+    name: "harshal" ,
+    age : 19 ,
+    email : "harshal@google.com"
+}
+console.log(JsUser.email)
